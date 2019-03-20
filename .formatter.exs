2406c92c47771660a290021e8de74253b498240b
Karol Słuszniak <ksluszniak@gmail.com>
2019-03-20 13:27:12 +0100

    Fix syntax error in .formatter.exs

--- .formatter.exs
+++ .formatter.exs
@@ -1,7 +1,7 @@
 [
   inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"],
   line_length: 100,
-  locals_without_parens: [emit_to: 1]
+  locals_without_parens: [emit_to: 1],
   export: [
     locals_without_parens: [emit_to: 1]
   ]
